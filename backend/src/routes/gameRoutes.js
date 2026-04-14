21f2f0528f0638c8024149cd230a6a9a25be74e9
devitachiui22 <kuandashopp@gmail.com>
2026-04-14 21:51:01 +0100

    Delete backend/src directory

--- backend/src/routes/gameRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const gameController = require('../controllers/gameController');
-const auth = require('../middlewares/authMiddleware');
-
-router.post('/score', auth, gameController.submitScore);
-router.post('/scan', auth, gameController.processScan);
-
-module.exports = router;
